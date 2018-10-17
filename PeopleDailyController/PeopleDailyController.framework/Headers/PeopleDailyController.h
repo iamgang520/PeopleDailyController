513dea460e06404530beb13ee6f53212439fe809
iamgang <5391519@qq.com>
2018-10-17 16:39:56 +0800

    创建版本 0.0.1

---
+++ PeopleDailyController/PeopleDailyController.framework/Headers/PeopleDailyController.h
@@ -0,0 +1,17 @@
+//
+//  PeopleDailyController.h
+//  PengpaiNewsController
+//
+//  Created by iamgang on 2018/10/17.
+//  Copyright © 2018年 iamgang. All rights reserved.
+//
+
+#import <TMWebFramework/TMWebViewController.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PeopleDailyController : TMWebViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
